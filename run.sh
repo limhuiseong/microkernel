36c8ef6f80a67fa3db210b11bbcaa4f20e6ac815
limhuiseong <limhuiseong.cs@gmail.com>
2025-09-04 10:56:50 +0900

    chore: Configure initial development environment

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -xue
+
+QEMU=qemu-system-riscv32
+
+$QEMU                   \
+    -machine virt       \
+    -bios default       \
+    -nographic          \
+    -serial mon:stdio   \
+    --no-reboot         \
