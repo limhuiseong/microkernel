eb81b07b283259b5ca343d37804fef9837740a25
limhuiseong <limhuiseong.cs@gmail.com>
2025-09-08 01:00:53 +0900

    feat: Implement shell

--- shell.c
+++ shell.c
@@ -2,7 +2,28 @@
 
 void main(void)
 {
-    *((volatile int *)0x80200000) = 0x1234;
-    while (1)
-        ;
+    while (1) {
+prompt:
+        printf("> ");
+        char cmdline[128];
+        for (int i = 0;; i++) {
+            char ch = getchar();
+            putchar(ch);
+            if (i == sizeof(cmdline) - 1) {
+                printf("command line too long\n");
+                goto prompt;
+            } else if (ch == '\r') {
+                printf("\n");
+                cmdline[i] = '\0';
+                break;
+            } else {
+                cmdline[i] = ch;
+            }
+        }
+
+        if (strcmp(cmdline, "exit") == 0)
+            exit();
+        else
+            printf("unknown command: %s\n", cmdline);
+    }
 }
